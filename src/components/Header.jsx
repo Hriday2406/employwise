b94eb1f9b005421061bd1a2cbf1870a72119a76a
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-11-14 00:33:19 +0530

    Header Styling v1.0 - Basic Styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center p-5 bg-primary text-white absolute top-0 left-0 w-screen">
+      <h1 className="text-3xl font-bold">EmployWise</h1>
+    </div>
+  );
+}
